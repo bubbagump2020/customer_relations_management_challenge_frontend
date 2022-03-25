4bce8344678e68f9ea498cda7316d0d38506605b
Kevin Bagnall <bubbagump2020@gmail.com>
2022-03-25 11:32:45 -0500

    Data Visualization p1
    Added Material UI for a UI framework and using their Table Component(s) to show the data returned by the API call

--- src/components/views/ClientCard.tsx
+++ src/components/views/ClientCard.tsx
@@ -1,15 +1,23 @@
 import React from 'react';
 import Client from "../../misc/Client";
+import {TableCell, TableRow} from "@mui/material";
 
 interface IClientCard {
     client:Client
 }
 
 const ClientCard:React.FC<IClientCard> = ({client}:IClientCard) => {
-    return(
-        <div>
 
-        </div>
+    return(
+        <TableRow>
+            <TableCell>{client.firstName}</TableCell>
+            <TableCell>{client.lastName}</TableCell>
+            <TableCell>{client.email}</TableCell>
+            <TableCell>{client.stage}</TableCell>
+            <TableCell>{client.phone}</TableCell>
+            <TableCell>{client.company}</TableCell>
+            <TableCell>{client.probability}</TableCell>
+        </TableRow>
     )
 }
 
